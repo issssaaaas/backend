29aca1f8656801c202460b95e7766541137db70b
isaaldoy <168334577+isaaldoy@users.noreply.github.com>
2024-04-30 20:38:28 +0000

    14 - user registration API

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
 
+// Routes
+import userRoutes from "./routes/userRoutes";
+
 dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
-
 app.get("/", (req, res) => {
   res.send("Server is running...");
 });
+
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
