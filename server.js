1a4f111c09ff0280addb8d72a0ddb1927393932a
issssaaaas <152148254+issssaaaas@users.noreply.github.com>
2024-05-01 23:07:41 +0300

    still 14

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./config/db";
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 // Routes
-import userRoutes from "./routes/userRoutes";
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 connectDB();
+
 const app = express();
+
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("Server is running...");
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
 });
 
-app.use("/api/users", userRoutes);
+app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
